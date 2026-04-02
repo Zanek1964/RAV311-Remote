f62b33a6ce7a75a2da84c7b700860551e6688448
Alain Raymond <115857857+alray31@users.noreply.github.com>
2026-04-01 20:16:39 -0400

    Update const.py

--- custom_components/rav311_remote/const.py
+++ custom_components/rav311_remote/const.py
@@ -1,47 +1,30 @@
 """RAV311 Remote IR codes and command factory.
 
 Compatible with Yamaha RX-V361, RX-V361BL, HTR-6030, HTR-6025.
-
-All codes use the Pioneer protocol with repeat=2, exactly matching
-the ESPHome remote_transmitter.transmit_pioneer configuration.
 """
-
-from homeassistant.components.infrared import RAWInfraredCommand
-from .pioneer import make_pioneer_command
-
-def make_command(code: YamahaCode) -> RAWInfraredCommand:
-    return make_pioneer_command(rc_code=int(code), repeat=2)
-
 from __future__ import annotations
 
 from enum import IntEnum
 
-from .pioneer import PioneerCommand
+from homeassistant.components.infrared import RAWInfraredCommand
 
-REPEAT = 2
+from .pioneer import make_pioneer_command
 
 
 class YamahaCode(IntEnum):
     """IR codes for receivers using the Yamaha RAV311 remote."""
 
-    # Power
     POWER_ON = 0x7E7E
     STANDBY = 0x7E7F
-
-    # Volume
     VOLUME_UP = 0x5E1A
     VOLUME_DOWN = 0x5E1B
     MUTE = 0x5E1C
-
-    # Misc
     SLEEP = 0x5E57
     AUDIO_SELECT = 0x5EC3
     MULTI_CH_IN = 0x5E87
     LEVEL = 0x5E86
     DISPLAY = 0x5EC2
     RETURN = 0x5EAA
-
-    # Input sources
     CD = 0x5E15
     MD_CDR = 0x5EC9
     TUNER = 0x5E16
@@ -50,27 +33,20 @@ class YamahaCode(IntEnum):
     DVR = 0x5E13
     V_AUX = 0x5E55
     XM = 0x5EB4
-
-    # Navigation / Search
     SEARCH_MENU = 0x5E84
     PRESET_CH_UP = 0x5E9D
     PRESET_CH_DOWN = 0x5E9C
     AE_CAT_RIGHT = 0x5E9E
     AE_CAT_LEFT = 0x5E9F
-
-    # DSP / Sound modes
     PROGRAM_LEFT = 0x5E59
     PROGRAM_RIGHT = 0x5E58
     ENHANCER = 0x5E94
     STRAIGHT = 0x5E56
     SURROUND_DECODE = 0x5E8D
     NIGHT = 0x5E95
-
-    # Numeric (only 8 was present in the ESPHome config)
     NUM_8 = 0x5EDD
 
 
-# Human-readable labels for each code (used in button entities)
 YAMAHA_CODE_LABELS: dict[YamahaCode, str] = {
     YamahaCode.POWER_ON: "Power On",
     YamahaCode.STANDBY: "Standby",
@@ -105,7 +81,6 @@ YAMAHA_CODE_LABELS: dict[YamahaCode, str] = {
     YamahaCode.NUM_8: "8",
 }
 
-# Input sources available for the select entity
 INPUT_SOURCES: dict[str, YamahaCode] = {
     "CD": YamahaCode.CD,
     "MD/CD-R": YamahaCode.MD_CDR,
@@ -118,7 +93,6 @@ INPUT_SOURCES: dict[str, YamahaCode] = {
     "Multi CH IN": YamahaCode.MULTI_CH_IN,
 }
 
-# Sound programs for the select entity
 SOUND_PROGRAMS: dict[str, YamahaCode] = {
     "Straight": YamahaCode.STRAIGHT,
     "Enhancer": YamahaCode.ENHANCER,
@@ -127,6 +101,6 @@ SOUND_PROGRAMS: dict[str, YamahaCode] = {
 }
 
 
-def make_command(code: YamahaCode) -> PioneerCommand:
-    """Create a PioneerCommand for the given Yamaha code."""
-    return PioneerCommand(rc_code=int(code), repeat=REPEAT)
+def make_command(code: YamahaCode) -> RAWInfraredCommand:
+    """Create a RAWInfraredCommand for the given Yamaha code."""
+    return make_pioneer_command(rc_code=int(code), repeat=2)
